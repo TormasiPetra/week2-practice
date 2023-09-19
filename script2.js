4eeb909db93e79ff16d76be777122c2916aba25c
TormasiPetra <tormasi.petra@gmail.com>
2023-09-19 20:30:32 +0200

    Full name logging function

---
+++ script2.js
@@ -0,0 +1,12 @@
+function logger (text) {
+    console.log (text)
+}
+
+function fullName (a, b){
+    let fn = 0
+    fn = a + " " + b 
+
+    return fn
+}
+
+logger (fullName('Petra', 'Tormasi'));
