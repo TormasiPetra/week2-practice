5eefa97144159f76689a25b6ae75196e583ad8f0
TormasiPetra <tormasi.petra@gmail.com>
2023-09-22 20:08:52 +0200

    Countries from outer source with API

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-function logger (text) {
-/*     console.log( "I am a function")
- */    console.log(text)
-}
-logger ('hello');
-logger ('ciao');
-logger ('szia');
-/* let text = hello */
+console.log ('loaded');
 
-function sumTwoNumber (a, b) { // camelCase
-    /*  let a = 
-        let b = */
-    console.log (a + b);
-}
+let rootElement = document.querySelector('#root');
 
-/* sumTwoNumber (5, 10);
+// console.log(rootElement)
+
+//let htmlContent = "<h2>Hello world, I'm here!</h2>";
+//rootElement.insertAdjacentHTML('beforeend', htmlContent);
+
+/* let counter = 0;
+
+while (counter < 10){
+    rootElement.insertAdjacentHTML('beforeend', htmlContent);
+    counter++; //adott szam erteket plusz eggyel noveli
+};
  */
-function sumOfTwoNumbers (x, y) {
-    let sum = 0;
-    sum = x + y;
 
-    return sum;
-}
+/* 
+for (let counter = 1; counter <= 10; counter++) {
+    rootElement.insertAdjacentHTML('beforeend', htmlContent);
+}*/
 
-logger(sumOfTwoNumbers (10, 5));
+/* let array = ["alma", "korte", "ananasz", "eper", "kiwi"]
 
+for (let index = 0; index < array.length; index++) {
+    rootElement.insertAdjacentHTML('beforeend', `<h2> ${array[index]} </h2>`);
+} */
+
+fetch('https://restcountries.com/v3.1/all')
+.then(function (response){
+    return response.json()
+})
+.then(function(data) {
+    console.log(data) //itt tudunk a kapott adattal dolgozni
+                      // a data egy 250 elemből álló array
+
+    for (let index = 0; index < data.length; index++) {
+        let countryName = data[index].name.common
+        let countrypop = data[index].population 
+        let countryarea = data[index].area
+
+        //csak a for cikluson belül lehet ezt a változót elérni 
+
+        rootElement.insertAdjacentHTML("beforeend", `
+        <div class="country">
+            <h2> ${countryName} </h2>
+            <p> population: ${countrypop} </p>
+            <p> area: ${countryarea} km2 </p>
+        </div>   
+        `)
+    }
+})
